6c68ed0d6f6cb0905368bb148bffd9941a2adc37
daniel.almeida <daniel.almeida@turbinekreuzberg.com>
2017-10-07 01:06:01 +0100

    Fixed error with string compare

--- src/CatchCpp/FileHandling/JsonLoadTest.cpp
+++ src/CatchCpp/FileHandling/JsonLoadTest.cpp
@@ -114,7 +114,9 @@ TEST_CASE("Open a file and check the imported state is correct","[open]") {
     unsigned int counter = 0;
     for (auto  &element:expectedScenes)
     {
-        REQUIRE(element["name"].get<string>().compare(result["scenes"][counter]["name"]) == 0);
+        std::string expectedName = element["name"].get<string>();
+        std::string name = result["scenes"][counter]["name"];
+        REQUIRE(expectedName.compare(name) == 0);
         std::cout << element << std::endl;
         counter++;
     }
