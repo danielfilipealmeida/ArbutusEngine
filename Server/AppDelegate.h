0f5cc962f0d7b6a6b7b3a876a7a1cbdb8d8a6756
daniel.almeida <daniel.almeida@turbinekreuzberg.com>
2017-11-18 15:10:10 +0000

    Server started
    Copied the HelloWorld project for starting the server App.

---
+++ Server/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Server
+//
+//  Created by Daniel Almeida on 17/11/2017.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Server/main.m
@@ -0,0 +1,12 @@
+//
+//  main.m
+//  Server
+//
+//  Created by Daniel Almeida on 17/11/2017.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
